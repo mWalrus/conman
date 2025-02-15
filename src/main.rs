369771f90680d39b88fa7fb82e43be583247825a
mWalrus <hellkvistoskar@protonmail.com>
2025-02-15 17:38:20 +0100

    chore: add FIXME

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,14 @@ mod git;
 mod ops;
 mod paths;
 
+// FIXME: If you decide to swap branches in the config it should be to an already
+//        existing branch. We should fail if the branch specified doesn't exist.
+//        this forces the user to checkout a new branch by using the `branch checkout`
+//        command. This allows us to better check and invalidate cache by writing it
+//        only on `save` and check it only on branch switches.
+//        One issue though is that if we manually through the config checkout another
+//        existing branch we might have to either skip validating cache or figure something
+//        else out.
 fn main() {
     tracing_subscriber::fmt::init();
 
